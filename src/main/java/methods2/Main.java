21b60f6db4525415ce20c1b04a2bd5ddb4ea910b
MuratOzc <matemi012@gmail.com>
2024-02-25 18:22:54 +0300

    Video 26

--- src/main/java/methods2/Main.java
+++ src/main/java/methods2/Main.java
@@ -7,11 +7,21 @@ public class Main {
         System.out.println(newMessage);
         int num = add(5,7);
         System.out.println(num);
+        int sum = addMultiple(7,5,7,9,6,2,1,0);
+        System.out.println(sum);
     }
 
     public static int add(int num1, int num2){
         int result = num1 + num2;
         return result;
+    }
+
+    public static int addMultiple(int... numbers){
+        int sum = 0;
+        for (int num:numbers){
+            sum += num;
+        }
+        return sum;
     }
 
     public static void delete(){
